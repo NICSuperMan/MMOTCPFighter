e7043713567e7c168c3ff0ff924103e93c95b181
NICSuperMan <101113990+NICSuperMan@users.noreply.github.com>
2025-03-12 05:33:20 +0900

    Update CallBack.h

--- TCPFighter_MMO/CallBack.h
+++ TCPFighter_MMO/CallBack.h
@@ -3,6 +3,6 @@
 #include <Windows.h>
 void CALLBACK CreatePlayer(void** ppOutClient, NETWORK_HANDLE handle, DWORD dwID);
 void CALLBACK RemoveClient_IMPL(void* pClient);
-BOOL CALLBACK PacketProc(void* pClient, BYTE byPacketType);
+BOOL CALLBACK PacketProc(void* pClient, BYTE byPacketType); 
 
 
